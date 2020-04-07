ce5058878283944f6d04336b89bf9ace2872f878
FinnStutzenstein <finn.stutzenstein@hotmail.de>
2020-04-07 13:19:25 +0200

    Little Changes

--- auth/entrypoint.sh
+++ auth/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export STORAGE_PORT="${STORAGE_PORT:-6379}"
+export MESSAGE_BUS_PORT="${MESSAGE_BUS_PORT:-6379}"
 
-# TODO: read optional ports from env variables: db_port and redis_port
 wait-for-it --timeout=10 "$STORAGE_HOST:$STORAGE_PORT"
 wait-for-it --timeout=10 "$MESSAGE_BUS_HOST:$MESSAGE_BUS_PORT"
 
