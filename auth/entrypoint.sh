a69e35d9c38d0af57d86c14759e3c7f5f391f557
FinnStutzenstein <finn.stutzenstein@hotmail.de>
2020-07-15 16:52:36 +0200

    fixed missing renamings

--- auth/entrypoint.sh
+++ auth/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export STORAGE_PORT="${STORAGE_PORT:-6379}"
+export CACHE_PORT="${CACHE_PORT:-6379}"
 export MESSAGE_BUS_PORT="${MESSAGE_BUS_PORT:-6379}"
 
-wait-for-it --timeout=10 "$STORAGE_HOST:$STORAGE_PORT"
+wait-for-it --timeout=10 "$CACHE_HOST:$CACHE_PORT"
 wait-for-it --timeout=10 "$MESSAGE_BUS_HOST:$MESSAGE_BUS_PORT"
 
 ./key-gen.sh
