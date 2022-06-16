b8030c13e807c0caf0c279b3dea431dc4b46f24c
GabrielInTheWorld <meyergabriel@live.de>
2022-06-16 17:00:16 +0200

    Update

--- auth/.eslintrc.js
+++ auth/.eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
         jest: true
     },
     extends: ['plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
-    files: ['*.ts'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         project: ['./tsconfig.json'],
