4a75675147486fb102ab9c214dcca9178ecf4c1f
Ralf Peschke <rpeschke@peschke-it.de>
2023-11-29 17:44:49 +0100

    hash with argon2

--- auth/libraries/pip-auth/authlib/hashing_handler.py
+++ auth/libraries/pip-auth/authlib/hashing_handler.py
@@ -3,23 +3,25 @@ import hashlib
 import secrets
 from typing import Optional
 
+import argon2
 
 HASH_WITH_SALT_LENGTH = 152
 
 
 class HashingHandler:
     def hash(self, to_hash: str, hash_reference: Optional[str] = None) -> str:
-        salt = self.__getSalt(hash_reference)
-        hash = hashlib.new("sha512", to_hash.encode())
-        hash.update(salt.encode())
-        hashValue = base64.b64encode(hash.digest()).decode("utf-8")
-        return salt + hashValue
+        ph = argon2.PasswordHasher(hash_len=152, encoding='utf-8')
+        return ph.hash(to_hash)
+        # hash = hashlib.new("sha512", to_hash.encode())
+        # hash.update(salt.encode())
+        # hashValue = base64.b64encode(hash.digest()).decode("utf-8")
+        # return salt + hashValue
 
-    def __getSalt(self, hash_reference: Optional[str] = None) -> str:
-        if (
-            isinstance(hash_reference, str)
-            and len(hash_reference) == HASH_WITH_SALT_LENGTH
-        ):
-            return hash_reference[0:64]
-        else:
-            return secrets.token_hex(32)
+    # def __getSalt(self, hash_reference: Optional[str] = None) -> str:
+    #     if (
+    #         isinstance(hash_reference, str)
+    #         and len(hash_reference) == HASH_WITH_SALT_LENGTH
+    #     ):
+    #         return hash_reference[0:64]
+    #     else:
+    #         return secrets.token_hex(32)
