da6d2a94c51e25f431d7ffe4841d7e73d8cdc229
Ralf Peschke <github@peschke-it.de>
2023-11-30 12:21:55 +0100

    Ba2026 argon2 exception catched (#171)

--- auth/libraries/pip-auth/authlib/hashing_handler.py
+++ auth/libraries/pip-auth/authlib/hashing_handler.py
@@ -3,9 +3,6 @@ from typing import Optional
 import argon2
 
 
-HASH_WITH_SALT_LENGTH = 152
-
-
 class HashingHandler:
     def hash(self, to_hash: str, hash_reference: Optional[str] = None) -> str:
         ph = argon2.PasswordHasher()
@@ -13,4 +10,11 @@ class HashingHandler:
 
     def verify(self, hash: str, password: str) -> bool:
         ph = argon2.PasswordHasher()
-        return ph.verify(hash, password)
+        try:
+            return ph.verify(hash, password)
+        except (
+            argon2.exceptions.VerifyMismatchError,
+            argon2.exceptions.VerificationError,
+            argon2.exceptions.InvalidHashError,
+        ):
+            return False
