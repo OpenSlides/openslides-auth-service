bd2e67c0e8301b072caca16799b8af04eb57e44c
Ralf Peschke <rpeschke@peschke-it.de>
2023-11-30 09:14:52 +0100

    password hasher without parameter seems to be the right one

--- auth/libraries/pip-auth/authlib/hashing_handler.py
+++ auth/libraries/pip-auth/authlib/hashing_handler.py
@@ -10,18 +10,9 @@ HASH_WITH_SALT_LENGTH = 152
 
 class HashingHandler:
     def hash(self, to_hash: str, hash_reference: Optional[str] = None) -> str:
-        ph = argon2.PasswordHasher(hash_len=152, encoding='utf-8')
+        ph = argon2.PasswordHasher()
         return ph.hash(to_hash)
-        # hash = hashlib.new("sha512", to_hash.encode())
-        # hash.update(salt.encode())
-        # hashValue = base64.b64encode(hash.digest()).decode("utf-8")
-        # return salt + hashValue
 
-    # def __getSalt(self, hash_reference: Optional[str] = None) -> str:
-    #     if (
-    #         isinstance(hash_reference, str)
-    #         and len(hash_reference) == HASH_WITH_SALT_LENGTH
-    #     ):
-    #         return hash_reference[0:64]
-    #     else:
-    #         return secrets.token_hex(32)
+    def verify(hash: str, password: str) -> bool:
+        ph = argon2.PasswordHasher()
+        return ph.verify(hash, password)
