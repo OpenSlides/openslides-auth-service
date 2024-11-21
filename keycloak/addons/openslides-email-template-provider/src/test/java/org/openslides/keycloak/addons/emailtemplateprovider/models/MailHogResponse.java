380fdb63f661895cf8a4330ef356ec6d34088d49
André Böhlke <ab@kryptance.de>
2024-11-21 11:54:50 +0100

    Work on test for email provider

---
+++ keycloak/addons/openslides-email-template-provider/src/test/java/org/openslides/keycloak/addons/emailtemplateprovider/models/MailHogResponse.java
@@ -0,0 +1,16 @@
+package org.openslides.keycloak.addons.emailtemplateprovider.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record MailHogResponse(
+        @JsonProperty("total") int total,
+        @JsonProperty("count") int count,
+        @JsonProperty("start") int start,
+        @JsonProperty("items") List<EmailItem> items
+) {}
+
+
