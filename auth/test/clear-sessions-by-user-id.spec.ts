81044a629e1e5895de1f63cc8df428af3d5efdfa
Hannes Janott <hannes.janott@openslides.com>
2024-12-05 11:46:06 +0100

    changes

--- auth/test/clear-sessions-by-user-id.spec.ts
+++ auth/test/clear-sessions-by-user-id.spec.ts
@@ -18,7 +18,7 @@ afterAll(async () => {
 });
 
 test('POST clear-sessions-by-user-id', async () => {
-    const admin = await container.request.login();
+    await container.request.login();
     await container.http.post('clear-sessions-by-user-id', {
         data: { userId: 1 }, 
         internal: true
