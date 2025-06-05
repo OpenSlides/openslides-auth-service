8508fb286096c7d3bd9d0f6687d44dd89cb79cfe
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-06-05 15:54:02 +0200

    Big Works?!

--- dev/run-tests.sh
+++ dev/run-tests.sh
@@ -10,11 +10,18 @@ CATCH=0
 PERSIST_CONTAINERS=$1
 
 make build-test || true
-echo "continue"
 CONTEXT="tests" docker compose -f docker-compose.dev.yml up -d || CATCH=1
 CONTEXT="tests" docker compose -f docker-compose.dev.yml exec -T auth ./wait-for.sh auth:9004 || CATCH=1
 CONTEXT="tests" docker compose -f docker-compose.dev.yml exec -T auth npm run test || CATCH=1
 CONTEXT="tests" docker compose -f docker-compose.dev.yml exec -T auth pytest || CATCH=1
+
+# Linters
+CONTEXT="tests" docker compose -f docker-compose.dev.yml exec -T auth npm run lint-check || CATCH=1
+CONTEXT="tests" docker compose -f docker-compose.dev.yml exec -T auth npm run prettify-check || CATCH=1
+CONTEXT="tests" docker compose -f docker-compose.dev.yml exec -w /app/libraries/pip-auth/ -T auth black --check --diff authlib/ tests/ || CATCH=1
+CONTEXT="tests" docker compose -f docker-compose.dev.yml exec -w /app/libraries/pip-auth/ -T auth isort --check-only --diff authlib/ tests/ || CATCH=1
+CONTEXT="tests" docker compose -f docker-compose.dev.yml exec -w /app/libraries/pip-auth/ -T auth flake8 authlib/ tests/ || CATCH=1
+CONTEXT="tests" docker compose -f docker-compose.dev.yml exec -w /app/libraries/pip-auth/ -T auth mypy authlib/ tests/ || CATCH=1
 
 if [ -z $PERSIST_CONTAINERS ]; then CONTEXT="tests" docker compose -f docker-compose.dev.yml down || CATCH=1; fi
 
