968769105bf9af64c8007bbe08d40a07a0c0cd25
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-06-26 11:01:31 +0200

    PR fixes #2

--- dev/command.sh
+++ dev/command.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-if [ ! -z "$dev"   ]; then node node_modules/.bin/nodemon src/index.ts; fi
-if [ ! -z "$tests" ]; then node node_modules/.bin/nodemon src/index.ts; fi
-if [ ! -z "$prod"  ]; then node index.js; fi
+if [ "$APP_CONTEXT" = "dev"   ]; then node node_modules/.bin/nodemon src/index.ts; fi
+if [ "$APP_CONTEXT" = "tests" ]; then node node_modules/.bin/nodemon src/index.ts; fi
+if [ "$APP_CONTEXT" = "prod"  ]; then node index.js; fi
